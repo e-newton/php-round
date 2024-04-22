4b1334090cd84082372916dd69d80fa9314cd1d5
Eric Newton <ericnewton2251@gmail.com>
2024-04-21 23:18:44 -0700

    Added in comments from repo

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,25 @@ function phpRound(
       : precision;
   const exponent = 10 ** Math.abs(precision);
 
+  /**
+	 * When extracting the integer part, the result may be incorrect as a decimal
+	 * number due to floating point errors.
+	 * e.g.
+	 * 0.285 * 10000000000 => 2849999999.9999995
+	 * floor(0.285 * 10000000000) => 2849999999
+	 *
+	 * Add 1 to the absolute value of the value adjusted by floor or ceil, use the
+	 * exponent to return it to its original precision, and compare it with value.
+	 * If it is equal to value, it is assumed that the absolute value is 1 smaller
+	 * due to error and will be corrected.
+	 * e.g.
+	 * 0.285 * 10000000000 => 2849999999.9999995
+	 * floor(0.285 * 10000000000) => 2849999999 (tmp_value)
+	 * tmp_value2 = 2849999999 + 1 => 2850000000
+	 * 2850000000 / 10000000000 == 0.285 => true
+	 * tmp_value = tmp_value2
+	 */
+
   if (value >= 0.0) {
     tmpValue = Math.floor(places > 0 ? value * exponent : value / exponent);
     tmpValue2 = tmpValue + 1;
@@ -32,12 +51,15 @@ function phpRound(
     tmpValue = tmpValue2;
   }
 
+  /* This value is beyond our precision, so rounding it is pointless */
   if (Math.abs(tmpValue) >= 1e16) {
     return value;
   }
 
+  /* round the temp value */
   tmpValue = phpRoundHelper(tmpValue, value, exponent, places, mode);
 
+  /* see if it makes sense to use simple division to round the value */
   if (Math.abs(places) < 23) {
     if (places > 0) {
       tmpValue = tmpValue / exponent;
@@ -45,7 +67,10 @@ function phpRound(
       tmpValue = tmpValue * exponent;
     }
   } else {
+    /* Simple division can't be used since that will cause wrong results.
+		   Instead, the number is converted to a string and back again */
     const convertedNumber = parseFloat(`${tmpValue}e${-places}`);
+    /* couldn't convert to string and back */
     if (!Number.isFinite(convertedNumber) || Number.isNaN(convertedNumber)) {
       tmpValue = value;
     }
